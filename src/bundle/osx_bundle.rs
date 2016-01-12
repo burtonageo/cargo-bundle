6e1f7337a84df08f94a00a4e789fe824ee339670
George Burton <burtonageo@gmail.com>
2016-01-12 08:04:23 +0000

    Remove double backslases from plist format

--- src/bundle/osx_bundle.rs
+++ src/bundle/osx_bundle.rs
@@ -7,8 +7,8 @@ use std::marker::{Send, Sync};
 const PLIST_TEMPLATE: &'static str = &"
     <?xml version=\"1.0\" encoding=\"UTF-8\"?>\
     <!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\
-    <plist version=\"1.0\"> \\
-    <dict> \\
+    <plist version=\"1.0\">\
+    <dict>\
         <key>CFBundleDevelopmentRegion</key>\
         <string>English</string>\
         <key>CFBundleExecutable</key>\
