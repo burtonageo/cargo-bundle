4b37eb8defe10873e8864adab84340e845dfbe43
George Burton <burtonageo@gmail.com>
2016-01-14 23:33:43 +0000

    Some formatting improvements

--- src/bundle/osx_bundle.rs
+++ src/bundle/osx_bundle.rs
@@ -28,56 +28,52 @@ pub fn bundle_project(settings: &Settings) -> Result<(), Box<Error + Send + Sync
                                 .map(|s| s.to_string())
                                 .ok_or(Box::from("Could not get file name of binary file.")));
 
-    {
-        // I wanted this to be a const &'static str, but unfortunately it has to be a string literal.
-        // Can't wait to bring on the const fns!
-        let contents = format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
-                                <!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \
-                                            \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n\
-                                <plist version=\"1.0\">\n\
-                                <dict>\n\
-                                    <key>CFBundleDevelopmentRegion</key>\n\
-                                    <string>English</string>\n\
-                                    <key>CFBundleExecutable</key>\n\
-                                    <string>{}</string>\n\
-                                    <key>CFBundleGetInfoString</key>\n\
-                                    <string></string>\n\
-                                    <key>CFBundleIconFile</key>\n\
-                                    <string>{}</string>\n\
-                                    <key>CFBundleIdentifier</key>\n\
-                                    <string></string>\n\
-                                    <key>CFBundleInfoDictionaryVersion</key>\n\
-                                    <string>6.0</string>\n\
-                                    <key>CFBundleLongVersionString</key>\n\
-                                    <string></string>\n\
-                                    <key>CFBundleName</key>\n\
-                                    <string>{}</string>\n\
-                                    <key>CFBundlePackageType</key>\n\
-                                    <string>APPL</string>\n\
-                                    <key>CFBundleShortVersionString</key>\n\
-                                    <string>{}</string>\n\
-                                    <key>CFBundleSignature</key>\n\
-                                    <string>{}</string>\n\
-                                    <key>CFBundleVersion</key>\n\
-                                    <string></string>\n\
-                                    <key>CSResourcesFileMapped</key>\n\
-                                    <true/>\n\
-                                    <key>LSRequiresCarbon</key>\n\
-                                    <true/>\n\
-                                    <key>NSHumanReadableCopyright</key>\n\
-                                    <string>{}</string>\n\
-                                </dict>\n\
-                                </plist>",
-                               bin_name,
-                               "", // icon file
-                               settings.bundle_name,
-                               settings.version_str.as_ref().unwrap_or(&settings.cargo_settings.version),
-                               settings.identifier,
-                               "" /* copyright*/);
+    let contents = format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
+                            <!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \
+                                        \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n\
+                            <plist version=\"1.0\">\n\
+                            <dict>\n\
+                                <key>CFBundleDevelopmentRegion</key>\n\
+                                <string>English</string>\n\
+                                <key>CFBundleExecutable</key>\n\
+                                <string>{}</string>\n\
+                                <key>CFBundleGetInfoString</key>\n\
+                                <string></string>\n\
+                                <key>CFBundleIconFile</key>\n\
+                                <string>{}</string>\n\
+                                <key>CFBundleIdentifier</key>\n\
+                                <string></string>\n\
+                                <key>CFBundleInfoDictionaryVersion</key>\n\
+                                <string>6.0</string>\n\
+                                <key>CFBundleLongVersionString</key>\n\
+                                <string></string>\n\
+                                <key>CFBundleName</key>\n\
+                                <string>{}</string>\n\
+                                <key>CFBundlePackageType</key>\n\
+                                <string>APPL</string>\n\
+                                <key>CFBundleShortVersionString</key>\n\
+                                <string>{}</string>\n\
+                                <key>CFBundleSignature</key>\n\
+                                <string>{}</string>\n\
+                                <key>CFBundleVersion</key>\n\
+                                <string></string>\n\
+                                <key>CSResourcesFileMapped</key>\n\
+                                <true/>\n\
+                                <key>LSRequiresCarbon</key>\n\
+                                <true/>\n\
+                                <key>NSHumanReadableCopyright</key>\n\
+                                <string>{}</string>\n\
+                            </dict>\n\
+                            </plist>",
+                           bin_name,
+                           "", // icon file
+                           settings.bundle_name,
+                           settings.version_str.as_ref().unwrap_or(&settings.cargo_settings.version),
+                           settings.identifier,
+                           "" /* copyright */);
 
-        try!(plist.write_all(&contents.into_bytes()[..]).map_err(Box::from));
-        try!(plist.sync_all().map_err(Box::from));
-    }
+    try!(plist.write_all(&contents.into_bytes()[..]).map_err(Box::from));
+    try!(plist.sync_all().map_err(Box::from));
 
     app_bundle_path.push("MacOS");
     try!(create_dir_all(&app_bundle_path).map_err(Box::from));
