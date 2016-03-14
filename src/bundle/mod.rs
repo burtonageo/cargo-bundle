0f0ccb35763efc65bf15c669ecdf811d713205f2
George Burton <burtonageo@gmail.com>
2016-03-14 19:52:38 +0000

    Some refactoring, and impl bundle_project for windows os

--- src/bundle/mod.rs
+++ src/bundle/mod.rs
@@ -18,16 +18,22 @@ pub fn bundle_project(settings: Settings) -> Result<(), Box<Error + Send + Sync>
 
 #[cfg(target_os = "windows")]
 pub fn bundle_project(settings: Settings) -> Result<(), Box<Error + Send + Sync>> {
-    unimplemented!();
+    match settings.package_type {
+        None => Err(Box::from("Native windows bundles not yet supported")),
+        Some(PackageType::Deb) => deb_bundle::bundle_project(&settings),
+        Some(PackageType::Rpm) => rpm_bundle::bundle_project(&settings),
+        Some(otherwise) => Err(Box::from(format!("Wrong bundle type {:?}, can only be either `deb`, `rpm` or `win`",
+                                         otherwise))),
+    }
 }
 
 #[cfg(not(any(target_os = "windows", target_os = "macos")))]
 pub fn bundle_project(settings: Settings) -> Result<(), Box<Error + Send + Sync>> {
     match settings.package_type {
+        None => deb_bundle::bundle_project(&settings).and_then(|_| rpm_bundle::bundle_project(&settings)),
         Some(PackageType::Deb) => deb_bundle::bundle_project(&settings),
         Some(PackageType::Rpm) => rpm_bundle::bundle_project(&settings),
-        None => deb_bundle::bundle_project(&settings).and_then(|_| rpm_bundle::bundle_project(&settings)),
-        Some(otherwise@_) => {
+        Some(otherwise) => {
             Err(Box::from(format!("Wrong bundle type {:?}, can only be either `deb` or `rpm`",
                                   otherwise)))
         }
