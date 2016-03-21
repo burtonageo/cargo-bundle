4a89eef2926c8914be9cb00331386ba7a467789d
George Burton <burtonageo@gmail.com>
2016-03-21 22:31:27 +0000

    Add initial deb bundle impl code (very incomplete)

--- src/bundle/deb_bundle.rs
+++ src/bundle/deb_bundle.rs
@@ -3,6 +3,26 @@ use std::error::Error;
 use std::marker::{Send, Sync};
 use std::path::PathBuf;
 
-pub fn bundle_project(_settings: &Settings) -> Result<Vec<PathBuf>, Box<Error + Send + Sync>> {
+const CONTROL_FILE_FMT: &'static str = "
+Package: {}
+Version: {}
+Architecture: {}
+Maintainer: {}
+Installed-Size: {}
+Depends: {}
+Suggests: {}
+Conflicts: {}
+Breaks: {}
+Replaces: {}
+Provides: {}
+Section: {}
+Priority: {}
+Homepage: {}
+Description: {}
+";
+
+pub fn bundle_project(settings: &Settings) -> Result<Vec<PathBuf>, Box<Error + Send + Sync>> {
+    let deb_path = settings.cargo_settings.project_out_directory.clone();
+
     unimplemented!();
 }
