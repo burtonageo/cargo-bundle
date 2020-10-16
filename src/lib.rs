1c3a039fb78edfae27c8699f129150a3bd7eada7
Yu Shimura <mail@yuhr.org>
2020-10-16 10:49:27 +0900

    Add canonicalization

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ lazy_static::lazy_static! {
 fn _resources_root() -> Option<PathBuf> {
     if std::env::var_os("CARGO").is_some() {
         let resources_root = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR")?);
-        Some(resources_root)
+        let canonical_resources_root = dunce::canonicalize(resources_root).ok()?;
+        Some(canonical_resources_root)
     } else {
         // TODO: support for other platforms
         #[cfg(target_os = "macos")]
@@ -15,21 +16,28 @@ fn _resources_root() -> Option<PathBuf> {
             let bundle = core_foundation::bundle::CFBundle::main_bundle();
             let bundle_path = bundle.path()?;
             let resources_path = bundle.resources_path()?;
-            let resources_root = bundle_path.join(resources_path);
-            Some(resources_root)
+            let absolute_resources_root = bundle_path.join(resources_path);
+            let canonical_resources_root = dunce::canonicalize(absolute_resources_root).ok()?;
+            Some(canonical_resources_root)
         }
     }
 }
 
-/// Returns the absolute path to the resources root, if available. It can handle the difference between `cargo run` and bundled run.
+/// Returns the absolute path to the resources root, if exists.
+///
+/// It can handle the difference between `cargo run` and bundled run. When in `cargo run`, it returns what `CARGO_MANIFEST_DIR` environment variable contains. When in bundled run, it varies:
+///
+/// - On macOS, [CFBundleCopyBundleURL](https://developer.apple.com/documentation/corefoundation/1537142-cfbundlecopybundleurl) and [CFBundleCopyResourcesDirectoryURL](https://developer.apple.com/documentation/corefoundation/1537113-cfbundlecopyresourcesdirectoryur) are used internally. Returned path will be `<bundle-path>/Contents/Resources`, where `<bundle-path>` is the absolute path to the generated `*.app` bundle.
+/// - Other platforms are not supported yet,
 pub fn resources_root() -> Option<PathBuf> {
     let resources_root = PathBuf::from((*RESOURCES_ROOT).as_ref()?);
     Some(resources_root)
 }
 
-/// Joins passed path to the resources root.
-pub fn resource_path<P: AsRef<Path>>(relative_from_resources_root: P) -> Option<PathBuf> {
+/// Returns the absolute form of given path relative to the resources root, if exists.
+pub fn resource_path<P: AsRef<Path>>(relative_to_resources_root: P) -> Option<PathBuf> {
     let resources_root = (*RESOURCES_ROOT).as_ref()?;
-    let resource_path = resources_root.join(relative_from_resources_root);
-    Some(resource_path)
+    let absolute_resource_path = resources_root.join(relative_to_resources_root);
+    let canonical_resource_path = dunce::canonicalize(absolute_resource_path).ok()?;
+    Some(canonical_resource_path)
 }
