cc5defd5b16dbf84ce5a0eade28daaa7cf0745ea
iamawacko <iamawacko@protonmail.com>
2022-07-10 20:48:45 -0700

    Added more functionality

--- src/bundle/flatpak_bundle.rs
+++ src/bundle/flatpak_bundle.rs
@@ -2,6 +2,7 @@ use super::common;
 use chrono;
 use dirs;
 use icns;
+use process::Command;
 use std::cmp::min;
 use std::ffi::OsStr;
 use std::fs::create_dir_all;
@@ -12,9 +13,15 @@ use std::path::{Path, PathBuf};
 use {ResultExt, Settings};
 
 pub fn bundle_project(settings: &Settings) -> ::Result<Vec<PathBuf>> {
-    generate(settings)?;
-    flatpak()?;
-    Ok(vec![PathBuf::from(settings.project_out_directory())])
+    if true {
+        generate(settings)?;
+        flatpak(true, settings)?;
+        return Ok(vec![PathBuf::from(settings.project_out_directory())]);
+    } else {
+        generate(settings)?;
+        flatpak(false, settings)?;
+        return Ok(vec![PathBuf::from(settings.project_out_directory())]);
+    }
 }
 
 fn generate(settings: &Settings) -> ::Result<()> {
@@ -62,13 +69,48 @@ fn create_desktop_file(settings: &Settings, path: &Path) -> ::Result<()> {
 fn create_flatpak_yml(path: &Path, template: &str, infix: Option<&str>, settings: &Settings) -> ::Result<()> {
     let mut path = PathBuf::from(path);
     path.push(format!("{}{}.yml", settings.bundle_identifier(), infix.unwrap_or("")));
-    todo!()
-}
-
-fn create_app_xml() {
- todo!();
-}
-
-fn flatpak() -> ::Result<()> {
     Ok(())
+}
+
+fn create_app_xml() {}
+
+fn flatpak(release: bool, settings: &Settings) -> ::Result<()> {
+    let flatpak_build_rel = settings.project_out_directory().join("bundle/flatpak/repo");
+    let flatpak_temp = match prepare_flatpak_temp(settings.project_out_directory()) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Could not prepare flatpak-temp");
+            std::process::exit(-1);
+        }
+    };
+
+    let mut c = Command::new("flatpak-builder");
+    c.current_dir(&flatpak_temp);
+    if release {
+        c.arg(format!("--repo={}", flatpak_build_rel.display()));
+        c.arg("--force-clean");
+        c.arg(format!("--state-dir={}/../state", flatpak_build_rel.display()));
+        c.arg(format!("{}/../{}", flatpak_build_rel.display(), settings.binary_arch()));
+        c.arg(format!("../data/{}.dev.yml", settings.bundle_identifier()));
+    }
+    let mut c = c.spawn()?;
+    c.wait()?;
+
+    let flatpak_file_name = format!("{}.flatpak", settings.bundle_name());
+
+    let mut c2 = Command::new("flatpak")
+        .current_dir(&flatpak_temp)
+        .arg("build-bundle")
+        .arg(format!("{}/repo", flatpak_build_rel.display()))
+        .arg(format!("../{}", flatpak_file_name))
+        .arg(settings.bundle_identifier())
+        .spawn()?;
+    c2.wait()?;
+    Ok(())
+}
+
+fn prepare_flatpak_temp(project_dir: &Path) -> ::Result<PathBuf> {
+    let flatpak_temp = project_dir.join("target/bundle/flatpak/flatpak-temp");
+
+    Ok(flatpak_temp)
 }
