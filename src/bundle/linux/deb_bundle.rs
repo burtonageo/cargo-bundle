93c11241aadba5c0ec4bad44251e5448cb20d3af
markisha64 <marko.grizelj03@gmail.com>
2023-09-25 16:40:39 +0200

    fix: Installed-Size

--- src/bundle/linux/deb_bundle.rs
+++ src/bundle/linux/deb_bundle.rs
@@ -109,7 +109,12 @@ fn generate_control_file(
     )?;
     writeln!(&mut file, "Version: {}", settings.version_string())?;
     writeln!(&mut file, "Architecture: {arch}")?;
-    writeln!(&mut file, "Installed-Size: {}", total_dir_size(data_dir)?)?;
+    // deb Installed-Size is size in bytes / 1024
+    writeln!(
+        &mut file,
+        "Installed-Size: {}",
+        total_dir_size(data_dir)? / 1024
+    )?;
     let authors = settings.authors_comma_separated().unwrap_or_default();
     writeln!(&mut file, "Maintainer: {authors}")?;
     if !settings.homepage_url().is_empty() {
