e6a9ff4a1fc4d857398ff7a3e4ab9ec1f417bdee
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-06-22 13:19:47 +0800

    update dependencies
    update example 'hello'

--- examples/hello/main.rs
+++ examples/hello/main.rs
@@ -1,25 +1,51 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release builds
 
-extern crate winit;
+//
+// https://docs.rs/winit/0.30.11/winit/#event-handling
+//
 
-use winit::{
-    dpi::PhysicalSize,
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 
-fn main() {
-    let events_loop = EventLoop::new();
-    let window = Window::new(&events_loop).unwrap();
-    window.set_title("Hello, World!");
-    window.set_inner_size(PhysicalSize::new(640, 480));
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
 
-    events_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            ..
-        } => *control_flow = ControlFlow::Exit,
-        _ => *control_flow = ControlFlow::Wait,
-    });
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if let Ok(window) = event_loop.create_window(Window::default_attributes()) {
+            window.set_title("Hello, World!");
+            self.window = Some(window);
+        } else {
+            eprintln!("Failed to create window");
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                if let Some(window) = self.window.as_ref() {
+                    window.request_redraw()
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let event_loop = EventLoop::new()?;
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
